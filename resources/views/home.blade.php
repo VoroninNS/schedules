955fc5c5ead96421c985bdd58c595106fa3f0549
nikita <VoroninNS@eruditor-group.com>
2022-05-19 20:02:33 +0300

    обновлена домашняя страница

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,18 @@
 <h1>Homepage</h1>
 <h2>Открытое API для получения расписания <a href="https://stankin.ru">Станкина</a></h2>
+<p>Узлы:</p>
+<ul>
+    <li>
+        /byWeek/{group}/{subgroup}
+        - расписание на неделю. <a href="http://schedules.website/byWeek/ИДБ-20-05/А/">Пример запроса</a>
+    </li>
+    <li>
+        /byDay/{group}/{subgroup}/{day}
+        - расписание на день. <a href="http://schedules.website/byDay/ИДБ-20-05/А/Среда">Пример запроса</a>
+    </li>
+    <li>
+        /all_groups
+        - все доступные группы. <a href="http://schedules.website/all_groups">Пример запроса</a>
+    </li>
+</ul>
+
